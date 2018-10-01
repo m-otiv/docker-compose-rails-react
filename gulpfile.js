5824a893f67a68707584b4258c644cd55d3e3646
Michele Maietta <maietta.michele@gmail.com>
2018-10-01 13:25:44 +0200

    use Browserify with livereload
    change Docker port to 80

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,39 @@
 const gulp = require('gulp');
-const webpack = require('webpack-stream');
-const webpackConfig = require('./webpack.config.prod.js');
-const named = require('vinyl-named');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const browserSync = require('browser-sync').create();
 
 const SOURCE_DIR = './app/assets';
 const DIST_DIR = './public/assets';
 
 gulp.task('js', function() {
-    return gulp.src(`${SOURCE_DIR}/javascripts/*.js`)
-        .pipe(named())
-        .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest(`${DIST_DIR}/js/`))
+    var b = browserify({
+        entries: [`${SOURCE_DIR}/javascripts/application.js`],
+    }).transform('babelify', {presets: ['es2015', 'react']}).transform('envify');
+
+    return b
+        .bundle()
+        .pipe(source('application.js'))
+        .pipe(gulp.dest(`${DIST_DIR}/js/`));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(`${SOURCE_DIR}/javascripts/**/*.js`, ['js']);
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: 'http://localhost',
+    });
+    // browserSync.init({
+    //     server: "./app"
+    // });
+});
+
+gulp.task('js-watch', ['js'], function(done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('watcher', function() {
+    gulp.watch(`${SOURCE_DIR}/javascripts/**/*.js`, ['js-watch']);
 });
 
 gulp.task('default', ['js', 'watch']);
+gulp.task('watch', ['browser-sync', 'watcher']);
